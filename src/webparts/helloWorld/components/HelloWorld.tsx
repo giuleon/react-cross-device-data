2fad0916355789dd94c5b3325869f854a25e9307
Giuliano De Luca <e73379@hugoboss.com>
2021-07-13 17:37:07 +0200

    ui improvements

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -2,24 +2,96 @@ import * as React from 'react';
 import styles from './HelloWorld.module.scss';
 import { IHelloWorldProps } from './IHelloWorldProps';
 import { escape } from '@microsoft/sp-lodash-subset';
-import { DefaultButton, PrimaryButton } from 'office-ui-fabric-react';
-import { MSGraphClient } from '@microsoft/sp-http';  
-import * as MicrosoftGraph from '@microsoft/microsoft-graph-types'; 
+import {
+  PrimaryButton,
+  TextField
+} from 'office-ui-fabric-react';
+import { MSGraphClient } from '@microsoft/sp-http';
+
+interface IUserData {
+  Theme: string;
+  Token: string;
+  Preference1: string;
+}
 
 const Dashboard: React.FunctionComponent<IHelloWorldProps> = (props) => {
-  const getData = async () => 
-  {
+  const [userSettings, setUserSettings] = React.useState<IUserData | null>(null);
+
+  const setUserData = async () => {
     props.context.msGraphClientFactory
-    .getClient()
-    .then((client: MSGraphClient) => {
-      client
-        .api("me/drive/special/approot:/CrossDeviceApp/settings.json:/content")
-        .version("v1.0")
-        .put('{"key": "value"}', (err, res) => {
-          console.log(err,res);
-        });
-    });
+      .getClient()
+      .then((client: MSGraphClient) => {
+        client
+          .api("me/drive/special/approot:/CrossDeviceApp/settings.json:/content")
+          .version("v1.0")
+          .put('{"Theme": "Dark", "Token": "123456789", "Preference1": "some value"}', (err, res) => {
+            console.log(err, res);
+          });
+      });
   };
+
+  const getUserData = async () => {
+    const msGraphClient = await props.context.msGraphClientFactory.getClient();
+    const result = await msGraphClient
+      .api("me/drive/special/approot:/CrossDeviceApp/settings.json?select=@microsoft.graph.downloadUrl")
+      .version("v1.0")
+      .get();
+    console.log(result);
+    const response = await fetch(`${result['@microsoft.graph.downloadUrl']}`);
+    console.log('response', response);
+    const userData: IUserData = await response.json();
+    console.log('userData', userData);
+    return userData;
+
+    // .then((client: MSGraphClient) => {
+    //   client
+    //     .api("me/drive/special/approot:/CrossDeviceApp/settings.json?select=@microsoft.graph.downloadUrl")
+    //     .version("v1.0")
+    //     .get(async (err, res) => {
+    //       console.log(err,res);
+    //       const response = await fetch(`${res['@microsoft.graph.downloadUrl']}`);
+    //       console.log('response', response);
+    //       const userData: IUserData = await response.json();
+    //       console.log('userData', userData);
+    //       return userData;
+    //     });
+    // });
+  };
+
+  const loadUserData = async () => {
+    return await getUserData();
+  };
+
+  React.useEffect(() => {
+    console.log('useEffect');
+    (async () => {
+      let data = await getUserData();
+      console.log('loadUserData', data);
+      setUserSettings(data);
+    })();
+  }, []);
+
+  let view: any = '';
+  if (userSettings !== null) {
+    view =
+      <div style={{backgroundColor: '#fff'}}>
+        <div className={styles.row}>
+          <div className={styles.column}>
+            <TextField label="Standard:" underlined placeholder="Enter text here" defaultValue={userSettings.Theme} />
+          </div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.column}>
+            <TextField label="Disabled:" underlined placeholder="Enter text here" defaultValue={userSettings.Token} />
+          </div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.column}>
+            <TextField label="Required:" underlined placeholder="Enter text here" defaultValue={userSettings.Preference1} />
+          </div>
+        </div>
+      </div>;
+  }
 
   return (
     <div className={styles.helloWorld}>
@@ -34,9 +106,11 @@ const Dashboard: React.FunctionComponent<IHelloWorldProps> = (props) => {
             </a>
           </div>
         </div>
+        {view}
         <div className={styles.row}>
           <div className={styles.column}>
-            <PrimaryButton text='Get current user data' onClick={getData} ></PrimaryButton>
+            <PrimaryButton text='Save current user data' onClick={setUserData} ></PrimaryButton>&#9;
+            <PrimaryButton text='Get current user data' onClick={getUserData} ></PrimaryButton>
           </div>
         </div>
       </div>
